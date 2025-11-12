c4da3ad147429a7d9a1d97e415fd31f7d40a18d0
dilfyscps <jayce.curran3@icloud.com>
2025-11-12 06:41:31 -0800

    other page

---
+++ docs/.vitepress/theme/components/PosterCard.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="poster-card">
+    <img :src="img" :alt="name" />
+    <div class="poster-info">
+      <p class="poster-name">{{ name }}</p>
+      <p>{{ description }}</p>
+      <a class="poster-btn" :href="link" target="_blank">Open Mega</a>
+      <span class="hidden-text">{{ name }}</span>
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  name: String,
+  img: String,
+  link: String,
+  description: String
+})
+</script>
--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,12 +1,10 @@
 import DefaultTheme from 'vitepress/theme'
+import PosterCard from './components/PosterCard.vue'
 import './custom.css'
 
 export default {
   ...DefaultTheme,
   enhanceApp({ app }) {
-    const link = document.createElement('link')
-    link.rel = 'stylesheet'
-    link.href = 'https://use.typekit.net/ddn4vkj.css' // Your Adobe font link
-    document.head.appendChild(link)
+    app.component('PosterCard', PosterCard)
   }
 }
